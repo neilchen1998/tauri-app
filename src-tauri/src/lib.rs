54e5243c813fa0cdf7e592e94594802ea13c454f
Neil Chen <neilchen1998@gmail.com>
2025-04-11 15:53:06 -0500

    decreases synthetic wait time

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -27,9 +27,7 @@ fn get_dropdown_options() -> Result<Vec<DropdownOption>, String> {
 fn process_dropdown_value(value: &str) -> String {
     println!("Selected value from frontend: {}", value);
 
-    let dur = std::time::Duration::from_secs(15);
-
-    std::thread::sleep(dur);
+    std::thread::sleep(std::time::Duration::from_millis(500));
 
     format!("Received: {}", value)
 }
